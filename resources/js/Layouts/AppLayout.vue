ccd6941383b3758cf5dd9ddaa13fce6924a4af55
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-05-30 00:49:11 +0200

    Update AppLayout.vue

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -53,7 +53,7 @@ const logout = () => {
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
+                                    Home
                                 </NavLink>
 
                                 <NavLink :href="route('apps')" :active="route().current('apps')">
@@ -211,7 +211,7 @@ const logout = () => {
                 <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
                     <div class="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
+                            Home
                         </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('apps')" :active="route().current('apps')">
                             Apps
